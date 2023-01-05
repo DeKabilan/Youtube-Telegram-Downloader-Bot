7e8bb69840d718504d9f3e440fa199da976062c5
DeKabilan <38588004+DeKabilan@users.noreply.github.com>
2023-01-05 20:25:40 +0530

    Error_Check

---
+++ main.py
@@ -0,0 +1,30 @@
+import telebot
+from dotenv import load_dotenv
+import os
+from yt import download
+import time
+
+load_dotenv(".env")
+tokens=os.getenv("tokens")
+tokens="5947596707:AAERSl_x9nNHpdqc4b_h9DdO8cUq1DY52Y4"
+bot=telebot.TeleBot(token=tokens)
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.send_message(message.chat.id,"Support the Dev: www.github.com/DeKabilan")
+    
+
+@bot.message_handler(func=lambda msg: msg)
+def sendlink(message):
+    if "youtu" in message.text:
+        bot.reply_to(message,download(message.text,"720p"))
+    else:
+        bot.reply_to(message,"Link is Invalid")
+
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(5)
+  
---
+++ yt.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+import pyshorteners
+from dotenv import load_dotenv
+import os
+load_dotenv(".env")
+apkey=os.getenv("apkey")
+apkey="4f05b12112626b8960b71013d87d28767b62e57d"
+
+
+
+def download(link,reso):
+
+    res=[]
+    video = YouTube(link)
+    for stream in video.streams:
+        if stream.resolution!=None:
+            res.append(stream.resolution)
+    res=set(res)
+    if reso in res:
+        downloads = video.streams.filter(progressive="True", file_extension="mp4", res=reso).first().url
+        s = pyshorteners.Shortener(api_key=apkey)
+        dl=(s.bitly.short(downloads))
+        return dl
+    else:
+        return "Resolution Not Found"
+
